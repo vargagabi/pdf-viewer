e536e9c9f2cd4ccf54e912b78e2f07072fc5d443
Varga Gábor <vargagabi2209@gmail.com>
2024-12-22 01:30:33 +0100

    add other pdf viewer

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,60 +1,58 @@
-import { isPlatformBrowser } from '@angular/common';
-import { AfterViewInit, Component, ElementRef, Inject, OnInit, PLATFORM_ID, ViewChild } from '@angular/core';
+import { CommonModule, isPlatformBrowser } from '@angular/common';
+import { AfterViewInit, ChangeDetectionStrategy, Component, Inject, inject, OnInit, PLATFORM_ID } from '@angular/core';
+import { DomSanitizer, SafeResourceUrl } from '@angular/platform-browser';
 import { RouterOutlet } from '@angular/router';
-import * as pdfjs from 'pdfjs-dist';
-import * as PDFJSViewer from 'pdfjs-dist/web/pdf_viewer.mjs';
+import { NgxExtendedPdfViewerModule, NgxExtendedPdfViewerService } from 'ngx-extended-pdf-viewer';
+
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [
+    RouterOutlet,
+    CommonModule,
+    NgxExtendedPdfViewerModule,
+  ],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
+  providers: [NgxExtendedPdfViewerService],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class AppComponent implements AfterViewInit {
-  title = 'pdf-viewer';
-
-  @ViewChild('canvas')
-  canvas!: ElementRef<HTMLCanvasElement>;
-
-  width: number = 600;
-  height: number = 900;
+export class AppComponent implements OnInit, AfterViewInit {
+  title: string = 'pdf-viewer';
+  sanitizer: DomSanitizer = inject(DomSanitizer);
+  pdfUrl?: SafeResourceUrl = undefined;
+  pdf: Blob | string | undefined = undefined;
+  isBrowser: boolean = false;
 
   constructor(@Inject(PLATFORM_ID) private platformId: object) {
+    this.isBrowser = isPlatformBrowser(this.platformId) && typeof window !== 'undefined' && typeof document !== 'undefined';
+    console.log(this.isBrowser);
   }
 
-  ngAfterViewInit(): void {
-    if (!isPlatformBrowser(this.platformId)) {
+  ngOnInit(): void {
+    if (!isPlatformBrowser(this.platformId) || typeof window === 'undefined' || typeof document === 'undefined') {
       return;
     }
-    let page: number = 1;
-
-    // from node_modules/pdfjs-dist/build/
-    // automatically copy on build?
-    pdfjs.GlobalWorkerOptions.workerSrc = 'pdf.worker.mjs';
-
-    const canvas: HTMLCanvasElement = this.canvas.nativeElement;
-    const context: CanvasRenderingContext2D = canvas.getContext('2d')!;
-
-    pdfjs.getDocument('A1_SD1_Wortliste_02.pdf').promise.then((pdf) => {
-      pdf.getPage(page).then((page) => {
-        const outputScale: number = 1.0;
-
-        const viewport = page.getViewport({ scale: outputScale });
-
-        canvas.width = Math.floor(viewport.width * outputScale);
-        canvas.height = Math.floor(viewport.height * outputScale);
-        canvas.style.width = Math.floor(viewport.width) + "px";
-        canvas.style.height = Math.floor(viewport.height) + "px";
-
-        const transform: any[] | undefined = outputScale !== 1
-          ? [outputScale, 0, 0, outputScale, 0, 0]
-          : undefined;
-
-        page.render({
-          canvasContext: context,
-          viewport: page.getViewport(viewport),
-          transform: transform,
-        });
-      });
-    });
+    this.pdf = localStorage.getItem('pdf') ?? undefined;
+    debugger;
   }
+  
+  ngAfterViewInit(): void {
+  }
+
+  public onFileChanged(event: Event): void {
+    if (!isPlatformBrowser(this.platformId) || typeof window === 'undefined' || typeof document === 'undefined') {
+      return;
+    }
+    const input = event.target as HTMLInputElement;
+    if (input.files && input.files.length > 0) {
+      const file = input.files[0];
+      // this.pdf = file;
+      const blob = new Blob([file], { type: file.type });
+      const url = URL.createObjectURL(blob);
+      this.pdf = url;
+      localStorage.setItem("pdf", url);
+      this.pdf = localStorage.getItem('pdf') ?? undefined;
+    }
+  }
+
 }
